87b0f7212b7e911958227ebd78d139ed2db7d152
DarkSideBTW <bengillitt@gmail.com>
2023-06-06 18:00:07 +0100

    NextJSFinished

--- Code/next-js-first-project-2/pages/[meetupId]/index.js
+++ Code/next-js-first-project-2/pages/[meetupId]/index.js
@@ -1,15 +1,80 @@
-import { Fragment } from 'react'
-import MeetupDetail from '@/components/meetups/MeetupDetail';
+import { MongoClient, ObjectId } from 'mongodb';
 
-function MeetupDetails() {
+import MeetupDetail from '@/components/meetups/MeetupDetail';
+import { Fragment } from 'react';
+import Head from 'next/head';
+
+function MeetupDetails(props) {
   return (
-    <MeetupDetail
-      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
-      title="First Meetup"
-      address="Some Street 5, Some City"
-      description="This is a first meetup"
-    />
+    <Fragment>
+      <Head>
+      <title>{props.meetupData.title}</title>
+        <meta
+          name="description"
+          content={props.meetupData.description}
+        />
+      </Head>
+      <MeetupDetail
+        image={props.meetupData.image}
+        title={props.meetupData.title}
+        address={props.meetupData.address}
+        description={props.meetupData.description}
+      />
+    </Fragment>
   );
+}
+
+export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    'MongoDBPrivateKey'
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
+  return {
+    fallback: false,
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  const client = await MongoClient.connect(
+    'MongoDBPrivateKey'
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: new ObjectId(meetupId),
+  });
+
+  console.log(selectedMeetup);
+
+  client.close();
+
+  return {
+    props: {
+      meetupData: {
+        id: selectedMeetup._id.toString(),
+        image: selectedMeetup.image,
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        description: selectedMeetup.description,
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
--- Code/next-js-first-project-2/pages/index.js
+++ Code/next-js-first-project-2/pages/index.js
@@ -1,32 +1,27 @@
+import { Fragment } from 'react';
+import Head from 'next/head';
 import MeetupList from '@/components/meetups/MeetupList';
-
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A First Meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 5, 12345 Some City',
-    description: 'This is a first meetup!',
-  },
-  {
-    id: 'm2',
-    title: 'A Second Meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 10, 12345 Some City',
-    description: 'This is a second meetup!',
-  },
-];
+import { MongoClient } from 'mongodb';
 
 function HomePage(props) {
-  return <MeetupList meetups={props.meetups} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />;
+    </Fragment>
+  );
 }
 
 // export async function getServerSideProps(context) {
 //   const req = context.req;
 //   const res = context.res;
-  
+
 //   // fetch data from API
 
 //   return {
@@ -39,11 +34,28 @@ function HomePage(props) {
 export async function getStaticProps() {
   // This is a reserved name
   // fetch data from an API
+
+  const client = await MongoClient.connect(
+    'MongoDBPrivateKey'
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        image: meetup.image,
+        address: meetup.address,
+        id: meetup._id.toString(),
+      })),
     },
-    revalidate: 1
+    revalidate: 1,
   };
 }
 
