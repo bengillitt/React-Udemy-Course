e897e650f2d7f848b7190f1a669a6a642ae4bb58
DarkSideBTW <bengillitt@gmail.com>
2023-06-10 10:06:01 +0100

    Finished ReactAnimations

--- Code/ReactAnimations/src/App.js
+++ Code/ReactAnimations/src/App.js
@@ -39,6 +39,12 @@ class App extends Component {
           timeout={1000}
           mountOnEnter
           unmountOnExit
+          onEnter={() => {console.log('onEnter')}}
+          onEntering={() => {console.log('onEntering')}}
+          onEntered={() => {console.log('onEntered')}}
+          onExit={() => {console.log('onExit')}}
+          onExiting={() => {console.log('onExiting')}}
+          onExited={() => {console.log('onExited')}}
         >
           {(state) => (
             <div
@@ -53,18 +59,8 @@ class App extends Component {
             />
           )}
         </Transition>
-        {/* ) : null} */}
-        {/* <Modal show={this.state.modalIsOpen} closed={this.closeModal} /> */}
-        <Transition
-          mountOnEnter
-          unmountOnExit
-          in={this.state.modalIsOpen}
-          timeout={300}
-        >
-          {(state) => <Modal show={state} closed={this.closeModal} />}
-        </Transition>
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
         {this.state.modalIsOpen ? <Backdrop show /> : null}
-        {/* <Backdrop show={this.state.showModal} /> */}
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
