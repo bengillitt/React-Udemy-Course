40df40ba3d99446a15df4d65419564de4cb934d5
DarkSideBTW <bengillitt@gmail.com>
2023-07-20 21:28:03 +0100

    Final Commit from react course

--- Code/Typescipt/react-ts/src/App.tsx
+++ Code/Typescipt/react-ts/src/App.tsx
@@ -1,9 +1,14 @@
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
+import TodosContextProvider from './store/todos-context';
 
 function App() {
   return (
     <div>
-      <Todos items={['Learn React', 'Learn Typescript']} />
+      <TodosContextProvider>
+        <NewTodo />
+        <Todos />
+      </TodosContextProvider>
     </div>
   );
 }
